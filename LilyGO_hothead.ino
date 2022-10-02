965a3a0806700931b3f383f425fbf502ee6e2ac7
teastainGit <80008504+teastainGit@users.noreply.github.com>
2022-10-02 16:53:38 -0400

    Update some graphics
    Graphics

--- LilyGO_hothead.ino
+++ LilyGO_hothead.ino
@@ -14,7 +14,8 @@ TFT_eSPI tft = TFT_eSPI();
 TFT_eSprite sprite = TFT_eSprite(&tft);
 
 void setup() {
-  Serial.begin(115200); // be sure to set USB CDC On Boot: "Enabled"
+  Serial.begin(115200);  // be sure to set USB CDC On Boot: "Enabled" 
+  //(Serial print slows progres bar Demo)
   tft.init();
   tft.setRotation(3);
   tft.setSwapBytes(true);
@@ -27,7 +28,7 @@ void setup() {
   sprite.setTextDatum(4);
   delay(500);
   Serial.println("In setup!");
-  delay(2000);
+  delay(1000);
   pinMode(14, INPUT); //Right button proven to be pulled up, push = 0
   pinMode(0, INPUT); //Left button proven to be pulled up, push = 0
 }
@@ -41,13 +42,13 @@ int y = 90;
 int blocks = 0;
 
 void loop() {
-  Serial.println("In loop!");
+  //Serial.println("In loop!"); //(Serial print slows progres bar Demo)
 
   sprite.fillSprite(TFT_WHITE);   // left side background colour
   sprite.setFreeFont(&Orbitron_Light_24);
-  if (!digitalRead(0) && !digitalRead(14))  // that old reverse logic push for "0"
+  if (!digitalRead(0) && !digitalRead(14))  //reverse logic push for "0"
   {
-    sprite.drawString(" BoneHead!", 75, 16);
+    sprite.drawString(" BoneHead!", 74, 16);
   }
   else {
     sprite.drawString("HotHead!", 75, 16);
@@ -57,7 +58,7 @@ void loop() {
   sprite.drawString(String(progress) + "%", 75, 54);
 
   progress++;
-  if (progress == 101)
+  if (progress >= 101)
     progress = 0;
   sprite.setTextColor(TFT_BLACK, TFT_WHITE);  //text colour
   blocks = progress / 5;
@@ -76,7 +77,7 @@ void loop() {
   sprite.drawRect(5, 124, 65, 22, TFT_BLACK);  //"left" and "right" text boxes
   sprite.drawRect(80, 124, 65, 22, TFT_BLACK);  
   if (digitalRead(0)) {                        //  normally "on" level "true". 
-    sprite.drawString("left pin0", 35, 134, 2);  // "Push" is active, level "false"
+    sprite.drawString("left pin0", 36, 134, 2);//"Push" is active, level "false"
   }
   else {
     sprite.drawString("LEFT", 30, 134, 2);
