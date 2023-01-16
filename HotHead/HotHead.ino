349fa41de2460fdc41f73a3a07d16698a7e7885c
teastainGit <80008504+teastainGit@users.noreply.github.com>
2023-01-15 20:31:09 -0500

    Tick! Tock!

--- HotHead/HotHead.ino
+++ HotHead/HotHead.ino
@@ -1,22 +1,23 @@
 
 #include "TFT_eSPI.h"
 #include "hothead.h"
-unsigned  colour = 0xFFFF;
+unsigned colour = 0xFFFF;
 TFT_eSPI tft = TFT_eSPI();
 #define topbutton 0
 #define lowerbutton 14
-#define PIN_POWER_ON 15 // LCD and battery Power Enable
-#define PIN_LCD_BL 38   // BackLight enable pin (see Dimming.txt)
+#define PIN_POWER_ON 15  // LCD and battery Power Enable
+#define PIN_LCD_BL 38    // BackLight enable pin (see Dimming.txt)
 int progress = 0;
 int x;
 int blocks = 0;
 int i = 0;
+bool ticktock = true;
 
 void setup() {
 
   pinMode(PIN_POWER_ON, OUTPUT);  //triggers the LCD backlight
   pinMode(PIN_LCD_BL, OUTPUT);    // BackLight enable pin
-  pinMode(lowerbutton, INPUT);     //Right button pulled up, push = 0
+  pinMode(lowerbutton, INPUT);    //Right button pulled up, push = 0
   pinMode(topbutton, INPUT);      //Left button  pulled up, push = 0
   delay(100);
   digitalWrite(PIN_POWER_ON, HIGH);
@@ -39,14 +40,17 @@ void setup() {
 }
 
 void loop() {
-  //Serial.print("progress =");     //(Serial print slows progress bar Demo)
-  Serial.println(progress);
-  tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  if (!digitalRead(topbutton) && !digitalRead(lowerbutton))
-  {
-    tft.drawString(" BoneHead!", 77, 55, 4);
+  ticktock = !ticktock;
+  if (ticktock) {
+    Serial.println("tick");  //(Serial print slows progress bar Demo)
+  } else {
+    Serial.println("tock");
   }
-  else {
+
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  if (!digitalRead(topbutton) && !digitalRead(lowerbutton)) {
+    tft.drawString(" BoneHead!", 77, 55, 4);
+  } else {
     tft.drawString("  HotHead!  ", 80, 55, 4);
   }
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
@@ -56,11 +60,11 @@ void loop() {
   if (progress >= 100)
     progress = 0;
   blocks = progress / 5;
-  tft.drawRoundRect(15, 110, 125, 18, 3, TFT_GREEN); //rectangle colour
+  tft.drawRoundRect(15, 110, 125, 18, 3, TFT_GREEN);  //rectangle colour
   // progress blocks below here
-  for ( i = 0; i < blocks; i++) {
-    x = i * 5 + i + 20;//x location i is increment 20 is a location offset
-     if (progress > 0 && progress < 40) {
+  for (i = 0; i < blocks; i++) {
+    x = i * 5 + i + 20;  //x location i is increment 20 is a location offset
+    if (progress > 0 && progress < 40) {
       colour = 0x001F;  //BLUE
     }
     if (progress > 40 && progress < 75) {
@@ -74,21 +78,19 @@ void loop() {
     }
     tft.fillRect(x, 114, 5, 10, colour);
   }
-  tft.drawRect(5, 5, 95, 22, TFT_BLUE);  //"Top"  text box
-  tft.drawRect(5, 146, 110, 22, TFT_BLUE); // "Bottom" text box
-  if (digitalRead(0)) {  //  normally open sw, pulled up.
+  tft.drawRect(5, 5, 95, 22, TFT_BLUE);     //"Top"  text box
+  tft.drawRect(5, 146, 110, 22, TFT_BLUE);  // "Bottom" text box
+  if (digitalRead(0)) {                     //  normally open sw, pulled up.
     tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  }
-  else {
+  } else {
     tft.setTextColor(TFT_RED, TFT_BLACK);
   }
   tft.drawString("< Top pin 0", 45, 15, 2);
   if (digitalRead(14)) {
     tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  }
-  else {
+  } else {
     tft.setTextColor(TFT_RED, TFT_BLACK);
   }
   tft.drawString("< Bottom p14", 55, 157, 2);
-  delay(30);
+  delay(100);
 }
