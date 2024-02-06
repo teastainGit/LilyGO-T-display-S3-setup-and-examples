35d4aa8ea4c7f90b29c7541b3e138cc3937c9fe2
teastainGit <80008504+teastainGit@users.noreply.github.com>
2024-02-05 19:17:37 -0500

    Update generic tick/tock example

--- Generic_tick_tock_serial_test/Generic_tick_tock_serial_test.ino
+++ Generic_tick_tock_serial_test/Generic_tick_tock_serial_test.ino
@@ -1,19 +1,17 @@
 //words preceded by "//"" are comments and are not executed
-bool ticktock; //declare ticktock as a boolean flag
+bool ticktock;  //declare ticktock as a boolean flag
 
-void setup() {          //runs once on start up
-pinMode(15, OUTPUT); 
-delay (500); 
-digitalWrite(15, HIGH);
-Serial.begin (115200); //open the serial port for USB cable
+void setup() {           //runs once on start up
+  Serial.begin(115200);  //open the serial port for USB cable
+  delay(500);
 }
 
-void loop() {         //runs in circles!
-ticktock = !ticktock; //every pass through reverse the flag
-  if (ticktock) {     //test current value of the ticktock flag
-    Serial.println("tick");  
-  } else {             //prints one or the other to the USB port
+void loop() {            //runs in circles!
+  ticktock = !ticktock;  //every pass through reverse the flag
+  if (ticktock) {        //test current value of the ticktock flag
+    Serial.println("tick");
+  } else {  //prints one or the other to the USB port
     Serial.println("tock");
   }
-delay (1000);           //wait for a second and run again!
+  delay(1000);  //wait for a second and run again!
 }
